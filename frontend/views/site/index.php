56f246c41bb4d20909d29b5c2c4f4b621b8af633
jaffrazz <jaffrazz@gmail.com>
2016-01-28 23:11:14 +0700

    more change FE

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -14,15 +14,13 @@ $this->title = 'My Yii Application';
 
         
     </div>
-    <section id="about">
-        <div class="col-lg-12 text-center">
+        <div class="col-lg-12 text-center about success" id="about">
             <h2>About Us</h2>
             <hr class="star-primary">
             <p class="lead">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum ipsum ut enim feugiat, ut semper est maximus. Vivamus tempus augue mi, ac aliquam ex egestas at. Morbi laoreet sapien turpis, quis vestibulum nulla mattis vel. Nunc tempus lorem tellus. Suspendisse euismod tempus feugiat. Duis non luctus nisi. Mauris pretium ipsum non sapien egestas ultricies. Vestibulum ut aliquet augue. Cras porttitor lectus eu massa elementum suscipit. Nunc metus felis, aliquet. 
             </p>                
     </div>
-    </section>
 
     
 </div>
