ac38c052a81bd98fcca9e108ace89698a47f63fa
Hafidz <gamerhacking@gmail.com>
2016-02-02 04:06:47 +0700

    Merge remote-tracking branch 'refs/remotes/origin/master' into API
    # Conflicts:
    #	composer.json
    #	composer.lock
    #	frontend/models/User.php
    #	vendor/composer/installed.json
    #	vendor/yiisoft/extensions.php

---
+++ api/common/libraries/Logger.php
@@ -0,0 +1,28 @@
+<?php
+namespace api\common\libraries;
+
+use yii;
+
+/**
+* 
+*/
+class Logger
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	/*
+	 * Write to table log
+	 * 
+	 * @param string $action
+	 */
+	public function write($action){
+		// $activity = new UserActivity();
+		// $activity->nik = Yii::$app->user->id;
+		// $activity->action = $action;
+		// $activity->save();
+	}
+}
