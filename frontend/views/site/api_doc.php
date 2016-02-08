4a3d828537f3dd875535ba85d699ad45d02a1726
jaffrazz <jaffrazz@gmail.com>
2016-02-09 00:21:15 +0700

    home v1.1 & doc v0.2

--- frontend/views/site/api_doc.php
+++ frontend/views/site/api_doc.php
@@ -30,12 +30,12 @@ $this->params['breadcrumbs'][] = $this->title;
                             ],
                             [
                                 'label' => 'Data Penduduk',
-                                'icon' => 'question-sign',
+                                'icon' => 'user',
                                 'url' => '#',
                             ],
                             [
                                 'label' => 'Data Statistik',
-                                'icon' => 'question-sign',
+                                'icon' => 'stats',
                                 'items' => [
                                     ['label' => 'Parameter Tahun', 'icon'=>'info-sign', 'url'=>'#'],
                                     ['label' => 'Parameter Lokasi', 'icon'=>'phone', 'url'=>'#'],
@@ -43,7 +43,7 @@ $this->params['breadcrumbs'][] = $this->title;
                             ],
                             [
                                 'label' => 'Data Kartu Keluarga',
-                                'icon' => 'question-sign',
+                                'icon' => 'credit-card',
                                 'items' => [
                                     ['label' => 'Pencarian Nomor KK', 'icon'=>'info-sign', 'url'=>'#'],
                                     ['label' => 'Pencarian NIK', 'icon'=>'phone', 'url'=>'#'],
@@ -58,16 +58,26 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="page-header bs-header">
                         <h1 id="sidenav-heading" class="text-warning">
                             <a class="kv-anchor" title="" href="#sidenav-heading" data-toggle="tooltip" data-original-title="Permalink">
-                                <span class="glyphicon glyphicon-link"></span>
-                            </a>SideNav Heading <small>SideNav::widget</small>
+                                <span class="glyphicon glyphicon-user"></span>
+                            </a> Data penduduk
                         </h1>
                     </div>
-                    <p>    You can control the side navigation heading by setting the parameter <code>heading</code></p><ul>
-        <li>You can set this to false to not display the heading. This is the default.</li>
-        <li>You can provide a plain text or a HTML Formatted string as the heading. This value is not HTML encoded.</li>
-        </ul><p></p>                        <div class="bs-example">
-                    <p class="lead">View this <a href="/sidenav-demo/profile/default">complete demo</a> that shows a complete usage of the Side Navigation menu with configurable options.</p>          </div>
-                <pre class="prettyprint linenums prettyprinted"><ol class="linenums"><li class="L0"><span class="com">// No Heading</span></li><li class="L1"><span class="pln">echo </span><span class="typ">SideNav</span><span class="pun">::</span><span class="pln">widget</span><span class="pun">([</span><span class="str">'items'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> $items</span><span class="pun">,</span><span class="pln"> </span><span class="str">'heading'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> </span><span class="kwd">false</span><span class="pun">]);</span></li><li class="L2"><span class="pln">    </span></li><li class="L3"><span class="com">// HTML Heading</span></li><li class="L4"><span class="pln">echo </span><span class="typ">SideNav</span><span class="pun">::</span><span class="pln">widget</span><span class="pun">([</span><span class="str">'items'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> $items</span><span class="pun">,</span><span class="pln"> </span><span class="str">'heading'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> </span><span class="str">'&lt;i class="glyphicon glyphicon-cog"&gt;&lt;/i&gt; Operations'</span><span class="pun">]);</span></li></ol></pre>
+                    <ul>
+                        <li>
+                            <h3>Request URI</h3>
+                            <pre class="prettyprint linenums prettyprinted">[GET] http://localhost/ocfa_yii/api/v1/penduduk?access-token=<access_token>&nik=<nik>&field=<fields></pre>
+                            <!-- <pre class="prettyprint linenums prettyprinted"><ol class="linenums"><li class="L0"><span class="com">// No Heading</span></li><li class="L1"><span class="pln">echo </span><span class="typ">SideNav</span><span class="pun">::</span><span class="pln">widget</span><span class="pun">([</span><span class="str">'items'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> $items</span><span class="pun">,</span><span class="pln"> </span><span class="str">'heading'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> </span><span class="kwd">false</span><span class="pun">]);</span></li><li class="L2"><span class="pln">    </span></li><li class="L3"><span class="com">// HTML Heading</span></li><li class="L4"><span class="pln">echo </span><span class="typ">SideNav</span><span class="pun">::</span><span class="pln">widget</span><span class="pun">([</span><span class="str">'items'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> $items</span><span class="pun">,</span><span class="pln"> </span><span class="str">'heading'</span><span class="pln"> </span><span class="pun">=&gt;</span><span class="pln"> </span><span class="str">'&lt;i class="glyphicon glyphicon-cog"&gt;&lt;/i&gt; Operations'</span><span class="pun">]);</span></li></ol></pre> -->
+                        </li>
+                        <li>
+                            <h3>Parameter</h3>
+                            <div class="col-md-2">access-token</div>
+                            <div class="col-md-10">Token akses anda</div>
+                            <div class="col-md-2">NIK</div>
+                            <div class="col-md-10">Nomor Induk Kependudukan</div>
+                            <div class="col-md-2">Field</div>
+                            <div class="col-md-10">Attribut pencarian dan setiap atribut dipisahkan dengan tanda '-'. Atribut yang diperbolehkan antara lain: nama, jenis_kelamin, tanggal_lahir, tempat_lahir, golongan_darah, kewarganegaraan, foto, agama, alamat, status_perkawinan, pekerjaan, pendidikan_terakhir</div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
